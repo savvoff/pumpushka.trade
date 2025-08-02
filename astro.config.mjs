0b268282119266e4888a2cc506b01c16569c578d
Ihor Savvov <ihor.savvov@fedevco.com>
2025-08-02 13:41:26 +0300

    Prettyfy code

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-   image: {
+  image: {
     service: passthroughImageService(),
   },
   output: 'static',
