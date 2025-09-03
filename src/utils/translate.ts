e71990c05daa944de2b35fb3f2067734b33288a2
Ihor Savvov <ihor.savvov@fedevco.com>
2025-09-03 16:24:23 +0300

    Fixed lang constant in utils

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -1,7 +1,7 @@
-import { DEFAULT_LANG } from 'src/constants';
+import { SITE } from 'src/constants';
 
 export function useI18n(lang: string) {
   return function t<T = string>(field: Record<string, T>): T {
-    return field?.[lang] ?? field?.[DEFAULT_LANG];
+    return field?.[lang] ?? field?.[SITE.defaultLanguage];
   }
 }
